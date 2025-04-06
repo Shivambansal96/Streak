0746540b1ae9616f18e936fabb10445938cacd03
Shivam Bansal <bansal.shivam1216@gmail.com>
2025-04-06 23:13:37 +0530

    change

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const header = document.getElementById("box");
+const main = document.getElementById("main");
 
 function changeCOlor() {
     document.querySelector("body").style.backgroundColor = "black"
 }
+
+main.style.backgroundColor = "red"
+
+
 
 
 changeCOlor();
